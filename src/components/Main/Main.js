ecca75acc4f99f8b8dfce92959cde25079d72ff6
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-10-11 00:19:49 +0600

    navbar added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Menubar from '../Manubar/Menubar';
 
 const Main = () => {
     return (
         <div>
-            <h2>Main</h2>
+            <Menubar></Menubar>
         </div>
     );
 };
