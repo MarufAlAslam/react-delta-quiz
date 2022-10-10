9d234cc948887b0d706f3c4cd81fa3cdb75b05ae
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-10-11 02:24:37 +0600

    banner added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import banner from './banner-img.svg';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner py-5'>
+            <div className='container'>
+                <div className='row column-reverse'>
+                    <div className='col-md-6 p-3 m-auto'>
+                        <h4>
+                            <span className='purple'>judge you with</span>
+                        </h4>
+                        <h1 className='fw-9 title'>
+                            Delta Quiz
+                        </h1>
+                        <h5>The Personalized quiz for programmers</h5>
+
+                        <ul className='list-unstyled mt-4'>
+                            <li className='py-2'>
+                                <span className='purple'>#</span> No. 1 Quiz Platform
+                            </li>
+                            <li className='py-2'>
+                                <span className='purple'>#</span> Specialized Questions
+                            </li>
+                            <li className='py-2'>
+                                <span className='purple'>#</span> Instant Answers
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div className='col-md-6 p-3'>
+                        <img src={banner} className="img-fluid" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
