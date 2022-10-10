9bc8b9bf042de877deb13e1eb3ef897a6decc8de
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-10-11 02:53:08 +0600

    footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer bg-light py-2'>
+            <div className='container'>
+                <div className='text-center'>
+                    <p className='mb-0'>
+                        &copy; 2022 Quiz App. All Rights Reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Menubar from '../Menubar/Menubar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Menubar></Menubar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
