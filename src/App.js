74b79063d5b72660ce771d4e7b80c58800fa69d1
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-10-11 13:37:39 +0600

    stat page with react recharts added

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
         {
           path: "/statistics",
           element: <Statistics></Statistics>,
+          loader: () => fetch("https://openapi.programming-hero.com/api/quiz")
         },
         {
           path: "/blogs",
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, BarChart, Bar, Line, ComposedChart, Area } from 'recharts';
+import './Statistics.css';
 
 const Statistics = () => {
+    const loaderData = useLoaderData()
+    let data = [];
+    loaderData.data.map(apiData => { data.push({ name: apiData.name, total: apiData.total }) })
+
     return (
-        <div>
-            <h2>Statistics</h2>
+        <div className='py-5'>
+            <div className='container'>
+                <div className='text-center mb-5'>
+                    <h2 className='purple'>Statistics</h2>
+                    <p className='mb-1'>Based on total available no. of quizes</p>
+                </div>
+
+                <ResponsiveContainer width="100%" height="100%">
+                    <ComposedChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 20,
+                            bottom: 20,
+                            left: 20,
+                        }}
+                    >
+                        <CartesianGrid stroke="#f5f5f5" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" fill="#413ea0" />
+                        <Line type="monotone" dataKey="total" stroke="red" activeDot={{ r: 15 }} strokeWidth={5} />
+                    </ComposedChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
